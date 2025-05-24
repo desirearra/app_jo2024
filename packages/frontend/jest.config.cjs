d406eb34fdf7f10f06d3111f89b0684c7e879c9d
Desire ARRA <desire.arra@lianeli.fr>
2025-05-24 05:13:35 +0200

    chore(config): update dependencies, config files and lockfiles

---
+++ packages/frontend/jest.config.cjs
@@ -0,0 +1,13 @@
+module.exports = {
+  testEnvironment: 'jsdom',
+  setupFilesAfterEnv: ['<rootDir>/src/setupTests.ts'],
+  moduleNameMapper: {
+    '^@/(.*)$': '<rootDir>/src/$1',
+    '\\.(css|less|scss|sass)$': 'identity-obj-proxy',
+    '\\.(gif|ttf|eot|svg|png)$': '<rootDir>/src/__mocks__/fileMock.ts',
+  },
+  transform: {
+    '^.+\\.(ts|tsx)$': 'babel-jest',
+  },
+  testMatch: ['**/__tests__/**/*.test.ts?(x)'],
+};
